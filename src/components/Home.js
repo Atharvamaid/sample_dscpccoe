accaabc9559d20966c395a11f10acd0f59e639fb
Dhanno2410 <dhanashreebhandari2410@gmail.com>
2021-01-07 13:56:22 +0530

    Cards Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import About from './AboutUs';
+import WhatWeDo from './WhatWeDo';
 function Home(props) {
     return(
         <div>
             <Header/>
-            
-            
+            <WhatWeDo/>
+
         </div>
     )
 }
