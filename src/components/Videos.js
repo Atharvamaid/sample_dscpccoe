a6acdef89a9ce313087dd6befe407a354239edbe
atharv123-wq <atharvamaid@gmail.com>
2021-01-16 22:05:11 +0530

    added events and video cards

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,93 +1,73 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+function MaterialCards(){
+  const classes = useStyles();
+  return(
+    <div>
+      <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="240"
+          image="https://i.picsum.photos/id/184/200/300.jpg?hmac=dCgm4a8do6DWvjUWcFvft3Kd1srf1f_TyIZoWGrgu48"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+    </div>
+  );
+}
+
 
 function Videos() {
     return (
         
-        <div className="row row-cols-1 px-4 row-cols-md-3 ">
-        <div className="col mb-4">        
-    <div className="card ">
-        <div className="view overlay">
-            <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/16.jpg"alt="Card image cap"/>
-                <a href="#!">
-                <div className="mask rgba-white-slight"></div>
-                </a>
-        </div>
-
-      
-      <div className="card-body">
-
-        <h4 className="card-title">Card title</h4>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-          card's content.</p>
-        <button type="button" className="btn btn-light-blue btn-md">Read more</button>
-
-      </div>
-</div>
-    </div>
-
-    <div className="col mb-4">  
-    <div className="card">
-        <div className="view overlay">
-            <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/16.jpg"alt="Card image cap"/>
-                <a href="#!">
-                <div className="mask rgba-white-slight"></div>
-                </a>
-        </div>
-
-      
-      <div className="card-body">
-
-        <h4 className="card-title">Card title</h4>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-          card's content.</p>
-        <button type="button" className="btn btn-light-blue btn-md">Read more</button>
-
-      </div>
-      </div>
-
-    </div>
-    <div className="col mb-4">  
-    <div className="card">
-        <div className="view overlay">
-            <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/16.jpg"alt="Card image cap"/>
-                <a href="#!">
-                <div className="mask rgba-white-slight"></div>
-                </a>
-        </div>
-
-      
-      <div className="card-body">
-
-        <h4 className="card-title">Card title</h4>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-          card's content.</p>
-        <button type="button" className="btn btn-light-blue btn-md">Read more</button>
-
-      </div>
-
-    </div>
-    </div>
-    <div className="col mb-4">  
-    <div className="card">
-        <div className="view overlay">
-            <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/16.jpg"alt="Card image cap"/>
-                <a href="#!">
-                <div className="mask rgba-white-slight"></div>
-                </a>
-        </div>
-
-      
-      <div className="card-body">
-
-        <h4 className="card-title">Card title</h4>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-          card's content.</p>
-        <button type="button" className="btn btn-light-blue btn-md">Read more</button>
-
-      </div>
-
-    </div>
-    </div>
+        <div className="container-fluid ">
+          <div className="row mt-3 pt-2 pb-5">
+            <div className="col-md-4 d-flex justify-content-center py-2">
+            <MaterialCards/>
+            </div>
+            <div className="col-md-4 d-flex justify-content-center py-2">
+            <MaterialCards/>
+            </div>
+            <div className="col-md-4 d-flex justify-content-center py-2">
+            <MaterialCards/>
+            </div>
+          </div>
+          
+        
     </div>
 
     )
