1309229f2fdc4c94f13cb78ac2a5fe215bdb902b
atharv123-wq <atharvamaid@gmail.com>
2021-01-23 15:31:08 +0530

    completed videos page

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,51 +1,48 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
+import InstagramEmbed from 'react-instagram-embed';
+
+
+
+const posts =[
+  {
+    url:'https://www.instagram.com/p/CEb1p9snVmT/?utm_source=ig_web_copy_link',
   },
-});
+  {
+    url:'https://www.instagram.com/p/CF94cxuhbaK/?utm_source=ig_web_copy_link',
+  },
+  {
+    url: 'https://www.instagram.com/p/CFt3sqYheFg/?utm_source=ig_web_copy_link'
+  },
+  {
+    url:'https://www.instagram.com/tv/CJApHt_hnRe/?utm_source=ig_web_copy_link'
+  },
+  {
+    url:'https://www.instagram.com/tv/CJDJpfjB_Cx/?utm_source=ig_web_copy_link'
+  },
+  {
+    url:'https://www.instagram.com/tv/CJDJpfjB_Cx/?utm_source=ig_web_copy_link'
+  }
+]
 
-function MaterialCards(){
-  const classes = useStyles();
+function InstaCards(props){
+  
   return(
     <div>
-      <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="240"
-          image="https://i.picsum.photos/id/184/200/300.jpg?hmac=dCgm4a8do6DWvjUWcFvft3Kd1srf1f_TyIZoWGrgu48"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+       <InstagramEmbed
+  url= {props.url}
+  clientAccessToken='221354173031286|870ed71497e6576727e7a0b651c7f2b9'
+  maxWidth="100%"
+  
+  hideCaption={true}
+  containerTagName='div'
+  protocol=''
+  injectScript
+  onLoading={() => {}}
+  onSuccess={() => {}}
+  onAfterRender={() => {}}
+  onFailure={() => {}}
+/>
     </div>
   );
 }
@@ -53,21 +50,18 @@ function MaterialCards(){
 
 function Videos() {
     return (
-        
-        <div className="container-fluid ">
-          <div className="row mt-3 pt-2 pb-5">
-            <div className="col-md-4 d-flex justify-content-center py-2">
-            <MaterialCards/>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center py-2">
-            <MaterialCards/>
-            </div>
-            <div className="col-md-4 d-flex justify-content-center py-2">
-            <MaterialCards/>
-            </div>
+      <div className="">
+      <div className="container-fluid  " >
+       <div className="row mt-4 mb-5  justify-content-around">
+      {
+        posts.map((member)=>(
+          <div className="col-md-4 mt-5 mb-5    justify-content-around">
+          <InstaCards  url = {member.url} />
           </div>
-          
-        
+        ))
+      }
+      </div>
+      </div>
     </div>
 
     )
